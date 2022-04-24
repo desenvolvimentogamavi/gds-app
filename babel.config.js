ce1dc51dfe1d136daa0d258c4e9dddb09c4a4507
flavioamann <flavio.mann@gamavi.com.br>
2022-04-24 13:58:03 -0300

    ajustes

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugin: [
-    [
-      'modele-resolver',
-      {
-        root: ['./src'],
-        extends: ['.ts, .tsx. js, .json'],
-        alias: {
-          '@assets': './src/assets',
-          '@components': './src/components',
-          '@features': './src/features',
-          '@navigation': './src/navigation',
-          '@styles': './src/styles',
-          '@templates': './src/templates',
-          '@utils': './src/utils',
-        },
-      },
-    ],
-  ],
 };
