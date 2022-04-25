801eaabb29c4242a492ae4c16b92f52e46eb2973
flavioamann <flavio.mann@gamavi.com.br>
2022-04-25 17:21:13 -0300

    pasta e arquivos deliveryFailed

---
+++ src/features/requests/screen/deliveryFailed/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Container} from './styles';
+
+const DeliveryFailedScreen = () => {
+  return (
+    <Container>
+      <Text>This is DeliveryFailedScreen!</Text>
+    </Container>
+  );
+};
+
+export default DeliveryFailedScreen;
